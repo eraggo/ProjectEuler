5bcb1fc4678ba88e0ee4c2d42d8d6031bab50832
Timo Heino <eraggo@gmail.com>
2015-08-14 16:03:00 +0300

    Created base code
    More info on last commit

---
+++ prob4/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(int); // true if input is palindrome
+bool factor(int); // true if can factorize input to 2 3-digit numbers
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
+
+bool isPalindrome(int in)
+{
+
+}
+
+bool factor(int in)
+{
+
+}

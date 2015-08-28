75633559e0cba4533384099b3d357c48ccb5a2a9
Timo Heino <eraggo@gmail.com>
2015-08-28 19:00:15 +0300

    Some tests with random numbers

--- prob12/main.cpp
+++ prob12/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+int countDivs(int); // how many divisors input has?
+bool canDivide(int,int); // not sure how big answer is yet
+
 int main(int argc, char const *argv[]) {
-        
+        std::cout << countDivs(73495) << std::endl;
         return 0;
+}
+
+int countDivs(int n)
+{
+        int amount=1;
+        for (int i=1; i<n; i++)
+        {
+                if (canDivide(n,i))
+                        amount++;
+        }
+        return amount;
+}
+
+bool canDivide(int n, int i)
+{
+        if (n%i)
+                return false;
+        return true;
 }

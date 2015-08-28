a29152574d0e6cfbf0dd5c617407d34e8b6fae8d
Timo Heino <eraggo@gmail.com>
2015-08-28 18:17:08 +0300

    Problem 14 ready

--- prob14/main.cpp
+++ prob14/main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 
-int calcChain(int);
-int next(int);
+int calcChain(long);
+long next(long);
 
 int main(int argc, char const *argv[])
 {
-        std::cout << "Chain:" << calcChain(5) << std::endl;
+        int longest=0;
+        int steps=0;
+        for (int i=2; i<=1000000; i++) {
+                if (calcChain(i)>steps) {
+                        steps=calcChain(i);
+                        longest=i;
+                }
+        }
+        std::cout << "Longest chain starts with:" << longest << std::endl;
         return 0;
 }
 
-int calcChain(int n)
+int calcChain(long n)
 {
-        int count=1;
-        while (n!=1)
-        {
+        int count=0;
+        while (n!=1) {
                 n=next(n);
                 count++;
         }
         return count;
 }
 
-int next(int n)
+long next(long n)
 {
         if (n%2)
                 return 3*n+1;

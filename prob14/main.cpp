445cc5c93afc6c47b824bc7ab34aa714f971497d
Timo Heino <eraggo@gmail.com>
2015-08-28 17:45:55 +0300

    Added chain counter
    calcChain counts how many iterations it does take to reach back to 1.

--- prob14/main.cpp
+++ prob14/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
+int calcChain(int);
 int next(int);
 
 int main(int argc, char const *argv[])
 {
-        std::cout << "next:" << next(34) << std::endl;
+        std::cout << "Chain:" << calcChain(5) << std::endl;
         return 0;
+}
+
+int calcChain(int n)
+{
+        int count=1;
+        while (n!=1)
+        {
+                n=next(n);
+                count++;
+        }
+        return count;
 }
 
 int next(int n)
